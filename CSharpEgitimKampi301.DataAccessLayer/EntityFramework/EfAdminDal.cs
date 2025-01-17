26e431aa38d90904607a7b61a267f49e126736ff
Gökay KUTLUK <gokaykutluk1@gmail.com>
2025-01-17 14:58:53 +0300

    18

---
+++ CSharpEgitimKampi301.DataAccessLayer/EntityFramework/EfAdminDal.cs
@@ -0,0 +1,19 @@
+﻿using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Repositories;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.EntityFramework
+{
+    public class EfAdminDal:GenericRepository<Admin>, IAdminDal
+    {
+    }
+}
+
+
+//ekle sil güncelle ıd ye göre getir
+//içinde a harfi geçmeye n kullanıcıları listele
---
+++ CSharpEgitimKampi301.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Repositories
+{
+    public class GenericRepository<t> : IGenericDal<t> where t : class
+    {   KampContext context = new KampContext();
+        private readonly DbSet<t> _object;
+        public GenericRepository()
+        {
+            _object = context.Set<t>();
+        }
+        public void Delete(t entity)
+        {
+          var deletedEntity=context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public List<t> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public t GetById(int id)
+        {
+            return _object.Find(id);
+
+        }
+
+        public void Insert(t entity)
+        {
+           var addedEntity=context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Update(t entity)
+        {
+            var updateEntity = context.Entry(entity);
+            updateEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
