a1f62bc2f74ef6409b59871217c080ee83ac40dd
Gökay KUTLUK <gokaykutluk1@gmail.com>
2025-02-04 22:38:24 +0300

    1

--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -76,6 +76,7 @@
             this.btnList.TabIndex = 3;
             this.btnList.Text = "Listele";
             this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
             // 
             // dataGridView1
             // 
@@ -151,6 +152,7 @@
             this.btnDelete.TabIndex = 12;
             this.btnDelete.Text = "Sil";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -160,6 +162,7 @@
             this.btnUpdate.TabIndex = 13;
             this.btnUpdate.Text = "Güncelle";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnGetById
             // 
@@ -169,6 +172,7 @@
             this.btnGetById.TabIndex = 14;
             this.btnGetById.Text = "ID\' ye Göre Getir";
             this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
             // 
             // FrmCategory
             // 
--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.BusinessLayer.Concrete;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +13,58 @@ using System.Windows.Forms;
 
 namespace CSharpEgitimKampi301.PresentationLayer
 {
+    
     public partial class FrmCategory : Form
     {
+        private readonly ICategoryService _categoryService;
+
         public FrmCategory()
         {
+            _categoryService = new CategoryManager(new DataAccessLayer.EntityFramework.EfCategoryDal());
             InitializeComponent();
+        }
+
+    
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.GetAll();
+            dataGridView1.DataSource=categoryValues;
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName = txtCategoryName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme Başarılı");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategoryId.Text);
+            var deletedValues = _categoryService.TGetById(id);
+            _categoryService.TDelete(deletedValues);
+            MessageBox.Show("Silme başarılı");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategoryId.Text);
+            var values = _categoryService.TGetById(id);
+            dataGridView1 .DataSource=values;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            int updatedId = int.Parse(txtCategoryId.Text);
+            var updatedValue = _categoryService.TGetById(updatedId);
+            updatedValue.CategoryName=txtCategoryName.Text;
+            updatedValue.CategoryStatus = true;
+            _categoryService.TUpdate(updatedValue);
         }
     }
 }
